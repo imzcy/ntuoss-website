354b70c758b1fec62a17c0d98161d4c762582682
Danyang <zhangdanyangg@gmail.com>
2013-07-28 11:25:19 +0800

    people page

--- library/handler/people.php
+++ library/handler/people.php
@@ -5,7 +5,11 @@ function _people() {
 	// Define variables
 	$r->bind('time', time());
 	$r->bind('title', 'People');
-	
+    $management_array = array("Prisident"=>"XX", "Honorable Secretory"=>"YY");
+    $r->bind('management_array', $management_array);
+    
+    $member = array("ABC", "BCD", "Ahmen", "Arthur");
+    $r->bind('member_array', $member);
 	// Render
 	$r->render('people');
 }
